a0cb341730c3171df9b700628534ac2c6ba4fac2
Andrew <MoonHeiz@yandex.by>
2025-07-05 01:15:24 +0300

    refactor: change grid to more consistant

--- src/components/servers.jsx
+++ src/components/servers.jsx
@@ -3,7 +3,7 @@ import { ServerList } from "@/components/server-list/server-list";
 export const Servers = ({ servers, serversLoading, hasMore, fetchNextServers }) => {
   const isServersForceLoading = serversLoading.loading && serversLoading.force;
   return (
-    <div className="grid relative grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 bg-base-300 py-4 pt-0 px-2 md:px-10 xl:px-14">
+    <div className="grid relative grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4 bg-base-300 py-4 pt-0 px-2 md:px-10 xl:px-14">
       {!isServersForceLoading && servers.length === 0 && (
         <div className="min-h-96 flex justify-center items-center flex-col col-span-full">
           <p className="text-center text-2xl font-extrabold">No results found</p>
