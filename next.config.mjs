4f5304332d4ba9aa3b70daefba4419f0cd8d8724
Andrew <MoonHeiz@yandex.by>
2025-08-20 13:00:28 +0300

    feat: reduce memory usage #3

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,17 @@ const nextConfig = {
   poweredByHeader: false,
   productionBrowserSourceMaps: false,
   output: "standalone",
+  experimental: {
+    webpackMemoryOptimizations: true,
+    webpackBuildWorkers: true,
+    serverSourceMaps: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
     unoptimized: true,
   },
