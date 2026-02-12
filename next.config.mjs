008248f80a9f32e5b024efcfbbf56356358035ed
Andrew <MoonHeiz@yandex.by>
2026-02-12 18:43:26 +0300

    refactor: remove polyfills

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,12 @@ const nextConfig = {
   experimental: { webpackMemoryOptimizations: true, serverSourceMaps: false, preloadEntriesOnStart: false },
   typescript: { ignoreBuildErrors: true },
   images: { unoptimized: true, qualities: [75, 100] },
+  turbopack: {
+    resolveAlias: {
+      "../build/polyfills/polyfill-module": "./src/lib/modern-polyfill.js",
+      "next/dist/build/polyfills/polyfill-module": "./src/lib/modern-polyfill.js",
+    },
+  },
 };
 
 export default nextConfig;
